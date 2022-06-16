a2e023a20d728d50930a588ae92ee58049dd15c4
Michael Phan <michaelphan35@gmail.com>
2022-06-15 20:01:35 -0700

    added .ds_store file

--- develop/index.js
+++ develop/index.js
@@ -1,2 +1,8 @@
+// Require Packages needed for this appliation // 
+
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require("./utils/markdown.js");
+
+// Array of questions //
+const questions =
